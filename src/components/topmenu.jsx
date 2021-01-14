8e587bf2f0f9f492ff3dfa414ec9d124cbfe1383
Usamasandhu <71733850+Usamasandhu@users.noreply.github.com>
2021-01-14 19:37:41 +0500

    n

--- src/components/topmenu.jsx
+++ src/components/topmenu.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 // import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
-import { AppBar, Button, FormControl, Toolbar, Typography } from '@material-ui/core';
+import { AppBar, Button, FormControl, Toolbar } from '@material-ui/core';
 import userService from '../services/UserService';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Form, Nav, Navbar} from 'react-bootstrap';
@@ -15,11 +15,12 @@ const TopMenu = () => {
     const classes = useStyles();
      return ( 
     
-    ////////////
     <Navbar bg="dark" variant="dark">
     <Nav className="mx-auto">
       <Nav.Link href="/" style={{color:"white"}}>Home</Nav.Link>
-      <Nav.Link href="/products" style={{color:"white"}}>Typography</Nav.Link>
+      <div className="form-group">
+        <input type="email" id="email" className="form-control" aria-describedby="emailHelp" placeholder="Email"></input>
+      </div>
       <Nav.Link href="/contact-us" style={{color:"white"}}>Contact Us</Nav.Link>
       {!userService.isLoggedin()? <>
         <Nav.Link href="/login" style={{color:"white"}}>Login</Nav.Link>
