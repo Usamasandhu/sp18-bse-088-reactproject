0a268c32be84c73f7cec51eeacc2205fa6f86527
Usamasandhu <71733850+Usamasandhu@users.noreply.github.com>
2021-01-15 04:58:07 +0500

    Update landingpage.jsx

--- src/components/landingpage.jsx
+++ src/components/landingpage.jsx
@@ -20,14 +20,16 @@ const LandingPage = () => {
             <Grid contained style={{backgroundColor:"whitesmoke"}}>
             <div>
             <h1 class = "logo" >
-              <img src=""  alt="Shpatu"/>
+              <img src="https://scontent.flhe6-1.fna.fbcdn.net/v/t1.0-9/95332357_116001500095523_7579334070060974080_n.jpg?_nc_cat=101&ccb=2&_nc_sid=85a577&_nc_ohc=l0vHuTcevXgAX9MC9s5&_nc_ht=scontent.flhe6-1.fna&oh=802255794047630e27d28c40267b3897&oe=602490E4"  alt="Shpatu"/>
             </h1>
-            <hr>
+            <div>
             <h1>S H P A T U</h1>
-            </hr>
+            </div>
+            <div>
             <h1 class = "log" >
-            <img src="" alt="" height="100%" width="100%"/>            
+            <img src="https://scontent.flhe6-1.fna.fbcdn.net/v/t1.0-9/94886131_116614573367549_5070463923791069184_n.jpg?_nc_cat=102&ccb=2&_nc_sid=dd9801&_nc_ohc=TMe0xUmZQNIAX9oJVL6&_nc_ht=scontent.flhe6-1.fna&oh=df55cabc4d134f65d586d787b7cf97ba&oe=60274E44" alt="" height="100%" width="100%"/>            
             </h1>
+            </div>
             </div>
             </Grid>
         </Grid>
