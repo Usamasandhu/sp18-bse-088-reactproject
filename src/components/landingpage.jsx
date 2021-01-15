cd9c265f205e7e39b357d9245c1c8d3ac37fc04a
Usamasandhu <71733850+Usamasandhu@users.noreply.github.com>
2021-01-15 05:20:49 +0500

    Update landingpage.jsx

--- src/components/landingpage.jsx
+++ src/components/landingpage.jsx
@@ -19,16 +19,19 @@ const LandingPage = () => {
         <Grid contained>
             <Grid contained style={{backgroundColor:"whitesmoke"}}>
             <div>
-            <h1 class = "logo" >
+            <h4 class = "logo" >
               <img src="https://scontent.flhe6-1.fna.fbcdn.net/v/t1.0-9/95332357_116001500095523_7579334070060974080_n.jpg?_nc_cat=101&ccb=2&_nc_sid=85a577&_nc_ohc=l0vHuTcevXgAX9MC9s5&_nc_ht=scontent.flhe6-1.fna&oh=802255794047630e27d28c40267b3897&oe=602490E4" class="rounded-circle mx-auto d-block" height="30%" width="30%" alt="Shpatu"/>
-            </h1>
-            <div>
-            <h1 class = "text-justify" >S H P A T U</h1>
-            </div>
+            </h4>
             <div>
             <h1 class = "log" >
-            <img src="https://scontent.flhe6-1.fna.fbcdn.net/v/t1.0-9/94886131_116614573367549_5070463923791069184_n.jpg?_nc_cat=102&ccb=2&_nc_sid=dd9801&_nc_ohc=TMe0xUmZQNIAX9oJVL6&_nc_ht=scontent.flhe6-1.fna&oh=df55cabc4d134f65d586d787b7cf97ba&oe=60274E44" alt="" class="rounded" height="100%" width="100%"/>            
+              <img src="https://scontent.flhe6-1.fna.fbcdn.net/v/t1.0-9/94886131_116614573367549_5070463923791069184_n.jpg?_nc_cat=102&ccb=2&_nc_sid=dd9801&_nc_ohc=TMe0xUmZQNIAX9oJVL6&_nc_ht=scontent.flhe6-1.fna&oh=df55cabc4d134f65d586d787b7cf97ba&oe=60274E44" alt="" class="rounded" height="100%" width="100%"/>            
             </h1>
+            </div>
+            <div>
+            <h1 class = "text-center" >Vision Statement</h1>
+            </div>
+            <div>
+            <p class = "text-center">We aim to continuously innovate to be the best destination for our customers and partners</p>
             </div>
             </div>
             </Grid>
