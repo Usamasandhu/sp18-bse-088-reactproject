dd934091630feecf693caab42ad0b0b5967ee73d
Usamasandhu <71733850+Usamasandhu@users.noreply.github.com>
2021-01-15 05:43:01 +0500

    Update topmenu.jsx

--- src/components/topmenu.jsx
+++ src/components/topmenu.jsx
@@ -17,7 +17,7 @@ const TopMenu = () => {
     
     <Navbar bg="dark" variant="dark">
     <Nav className="mx-auto">
-      <Nav.Link href="/"><img src="https://scontent.flhe6-1.fna.fbcdn.net/v/t1.0-9/95332357_116001500095523_7579334070060974080_n.jpg?_nc_cat=101&ccb=2&_nc_sid=85a577&_nc_ohc=l0vHuTcevXgAX9MC9s5&_nc_ht=scontent.flhe6-1.fna&oh=802255794047630e27d28c40267b3897&oe=602490E4" class="rounded-circle" height="1%" width="5%" alt="Shpatu"/></Nav.Link>
+      <Nav.Link href="/"><img src="https://scontent.flhe6-1.fna.fbcdn.net/v/t1.0-9/95332357_116001500095523_7579334070060974080_n.jpg?_nc_cat=101&ccb=2&_nc_sid=85a577&_nc_ohc=l0vHuTcevXgAX9MC9s5&_nc_ht=scontent.flhe6-1.fna&oh=802255794047630e27d28c40267b3897&oe=602490E4" class="rounded-circle" height="0.2%" width="1%" alt="Shpatu"/></Nav.Link>
       <Nav.Link href="/"  style={{textAlign:"left",marginLeft:"20px",marginRight:"80px",color:"white"}}><strong>SHPATU</strong></Nav.Link>
       <Nav.Link href="/products" style={{marginRight:"30px",color:"white"}}>Products</Nav.Link>
       <div className="form-group">
